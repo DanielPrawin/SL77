558f552850de394007e23dd81989044ba05bc48c
DanielPrawin <44159183+DanielPrawin@users.noreply.github.com>
2023-09-06 22:03:29 +0530

    Update cicd.groovy2

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,3 +2,14 @@ def newGit(repo)
 {
   git "https://github.com/DanielPrawin/${repo}.git"
 }
+def build()
+{
+   sh "mvn package"
+}
+def deploy(jobName,ip,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobName}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+def testing(testing){
+    sh "java -jar /var/lib/jenkins/workspace/ScriptedPipeLine/${testing}.jar"
+}
